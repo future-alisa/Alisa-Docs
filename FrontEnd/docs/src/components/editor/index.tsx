8b509a3dcba824323d23cea8c829d517acf2b1b6
godv <weijiangforevergod@outlook.com>
2023-09-04 20:23:04 +0800

    #集成Slate框架

---
+++ FrontEnd/docs/src/components/editor/index.tsx
@@ -0,0 +1,27 @@
+//this is important, because we need to point out the component is client or server one while redering.
+'use client';
+// Import React dependencies.
+import React, { useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
+
+const initialValue:any = [
+    {
+        type: 'paragraph',
+        children: [{ text: 'A line of text in a paragraph.' }],
+    },
+]
+
+const DefineEditor = () => {
+    const [editor] = useState(() => withReact(createEditor()))
+    return (
+        // Add the editable component inside the context.
+        <Slate editor={editor} initialValue={initialValue}>
+            <Editable />
+        </Slate>
+    )
+}
+
+export default DefineEditor;
