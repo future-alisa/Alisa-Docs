63c16fdf5c8f7b64c3c9aa8b3b932cec66d09dfb
SunYubo <sunyubo.alisa@outlook.com>
2023-09-04 16:06:00 +0800

    添加后端权限

---
+++ BackEnd/docs/src/main/java/com/alisa/docs/DocsApplication.java
@@ -0,0 +1,13 @@
+package com.alisa.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsApplication.class, args);
+	}
+
+}
