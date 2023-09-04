dc2cd9d360e0a7fd2f91686ed2242c79f83ed2a2
godv <weijiangforevergod@outlook.com>
2023-09-04 20:50:00 +0800

    #pages路由测试

--- FrontEnd/docs/src/components/editor/index.tsx
+++ FrontEnd/docs/src/components/editor/index.tsx
@@ -3,11 +3,11 @@
 // Import React dependencies.
 import React, { useState } from 'react'
 // Import the Slate editor factory.
-import { createEditor } from 'slate'
+import { Descendant, createEditor } from 'slate'
 // Import the Slate components and React plugin.
 import { Slate, Editable, withReact } from 'slate-react'
 
-const initialValue:any = [
+const initialValue: any[] = [
     {
         type: 'paragraph',
         children: [{ text: 'A line of text in a paragraph.' }],
