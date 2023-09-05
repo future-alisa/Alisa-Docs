57c4966db57f555f83af4b4f299d2ce1719f6dec
sunyubo <sunyubo.alisa@outlook.com>
2023-09-05 20:49:05 +0800

    添加sass，并使用css module

--- FrontEnd/docs/src/components/navigator.tsx
+++ FrontEnd/docs/src/components/navigator.tsx
@@ -1,10 +1,11 @@
+import styles from '../css/common.module.scss';
 function MyCircyIcon(){
-    return <button>but1</button>
+    return <button className={styles.btn}>but1</button>
 }
 
 export default function MyNavigator(){
     return (
-        <div>
+        <div className={styles.nav}>
             <MyCircyIcon></MyCircyIcon>
         </div>
     );
